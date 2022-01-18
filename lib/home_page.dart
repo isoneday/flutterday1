bc3801f9dbdfba60014623207c8cc9c2c31f702b
isoneday <iswandi.saputra11@gmail.com>
2022-01-18 10:24:03 +0700

    login page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_first_flutterapp/beranda_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,6 +9,9 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+//deklarasi variable
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -51,93 +55,91 @@ class _HomePageState extends State<HomePage> {
         ),
       ),
       body: SingleChildScrollView(
-        child: Center(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(
-              height: 30,
-            ),
-            // Image.asset(
-            //   "assets/gambar/logoina.png",
-            //   color: Colors.red,
-            //   width: 300,
-            //   height: 300,
-            // ),
-            Image.network(
-              "https://www.gamelab.id/uploads/modules/NEWS/671/maxresdefault.jpg?1618217239336",
-              height: 200,
-              width: double.infinity,
-              fit: BoxFit.fill,
-            ),
-            const Padding(
-              padding: EdgeInsets.only(top: 20),
-              child: Text(
-                "Flutter Basic",
-                style: TextStyle(
-                    fontSize: 35,
-                    color: Colors.red,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: "batmfo__"),
+        child: Form(
+          key: _formKey,
+          child: Center(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 30,
               ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const Padding(
-              padding: EdgeInsets.only(left: 30, right: 30),
-              child: TextField(
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(hintText: "input email anda"),
+              // Image.asset(
+              //   "assets/gambar/logoina.png",
+              //   color: Colors.red,
+              //   width: 300,
+              //   height: 300,
+              // ),
+              Image.network(
+                "https://www.gamelab.id/uploads/modules/NEWS/671/maxresdefault.jpg?1618217239336",
+                height: 200,
+                width: double.infinity,
+                fit: BoxFit.fill,
               ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            const Padding(
-              padding: EdgeInsets.only(left: 30, right: 30),
-              child: TextField(
-                keyboardType: TextInputType.number,
-                obscureText: true,
-                decoration: InputDecoration(hintText: "input password anda"),
+              const Padding(
+                padding: EdgeInsets.only(top: 20),
+                child: Text(
+                  "Flutter Basic",
+                  style: TextStyle(
+                      fontSize: 35,
+                      color: Colors.red,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: "batmfo__"),
+                ),
               ),
-            ),
-            const Padding(
-              padding: EdgeInsets.only(left: 30, right: 30),
-              child: TextField(
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(hintText: "input email anda"),
+              const SizedBox(
+                height: 20,
               ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            const Padding(
-              padding: EdgeInsets.only(left: 30, right: 30),
-              child: TextField(
-                keyboardType: TextInputType.number,
-                obscureText: true,
-                decoration: InputDecoration(hintText: "input password anda"),
+              Padding(
+                padding: EdgeInsets.only(left: 30, right: 30),
+                child: TextFormField(
+                  validator: validasiEmail,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    hintText: "input email anda",
+                    prefixIcon: Icon(
+                      Icons.email,
+                      color: Colors.red[900],
+                    ),
+                  ),
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Container(
-                  height: 50,
-                  width: double.infinity,
-                  child: ElevatedButton(
-                      style: ButtonStyle(
-                          shape:
-                              MaterialStateProperty.all(RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20),
-                        // side: const BorderSide(
-                        //     color: Colors.blue, width: 8)
-                      ))),
-                      onPressed: () {},
-                      child: Text("OK"))),
-            )
-          ],
-        )),
+              const SizedBox(
+                height: 10,
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 30, right: 30),
+                child: TextFormField(
+                  validator: validasiPassword,
+                  keyboardType: TextInputType.number,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      hintText: "input password anda",
+                      prefixIcon: Icon(Icons.lock, color: Colors.red[900])),
+                ),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Container(
+                    height: 50,
+                    width: double.infinity,
+                    child: ElevatedButton(
+                        style: ButtonStyle(
+                            shape: MaterialStateProperty.all(
+                                RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                          // side: const BorderSide(
+                          //     color: Colors.blue, width: 8)
+                        ))),
+                        onPressed: () {
+                          cekValidasi();
+                        },
+                        child: Text("OK"))),
+              )
+            ],
+          )),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.red,
@@ -150,5 +152,32 @@ class _HomePageState extends State<HomePage> {
         },
       ),
     );
+  }
+
+//validasi untuk memeriksa email inputan valid atau tidak
+  String? validasiEmail(String? value) {
+    String pattern =
+        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+    RegExp regex = new RegExp(pattern);
+    if (!regex.hasMatch(value!.trim()))
+      return 'Enter Valid Email';
+    else
+      return null;
+  }
+
+//validasi untuk memeriksa panjang karakter dari inputkan password
+  String? validasiPassword(String? value) {
+    if (value!.length < 6) {
+      return ' password harus lebih besar dari 5 karakter';
+    } else {
+      return null;
+    }
+  }
+
+  void cekValidasi() {
+    if (_formKey.currentState!.validate()) {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => BerandaPage()));
+    }
   }
 }
